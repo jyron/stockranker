c6a190e17be631c640abe655a96972c4d8e468a6
jyron <jyron.dev@gmail.com>
2023-04-10 01:46:02 -0500

    Rebuild atop simple fastapi-beanie-jwt implementation

---
+++ backend/myserver/main.py
@@ -0,0 +1,24 @@
+"""
+Server main runtime
+"""
+
+# pylint: disable=unused-import
+
+from myserver import jwt
+from myserver.app import app
+from myserver.routes.auth import router as AuthRouter
+from myserver.routes.mail import router as MailRouter
+from myserver.routes.register import router as RegisterRouter
+from myserver.routes.user import router as UserRouter
+
+
+app.include_router(AuthRouter)
+app.include_router(MailRouter)
+app.include_router(RegisterRouter)
+app.include_router(UserRouter)
+
+
+@app.get("/")
+async def root():
+    """Root endpoint"""
+    return {"message": "Hello World"}
